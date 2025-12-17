75cf2d97b6e557c3b91d6939d752ece60f2837a9
Mist-wu <2550166270@qq.com>
2025-12-17 13:14:42 +0800

    add black white list

--- config/config.py
+++ config/config.py
@@ -14,3 +14,17 @@ deepseek_api_key = os.getenv("DEEPSEEK_API_KEY")
 napcat_token = os.getenv("NAPCAT_TOKEN")
 
 root_user = "2550166270"
+
+# 群聊白名单：允许bot工作的群号
+group_whitelist = [
+    1051660592,
+]
+
+# 用户白名单：允许私聊唤醒bot的用户
+user_whitelist = [
+    root_user,
+]
+
+# 用户黑名单：无论群聊还是私聊，禁止bot回复这些用户
+user_blacklist = [
+]
