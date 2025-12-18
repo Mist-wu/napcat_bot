b9128ae8322c0eebba1a245d8d071b4d4e6ea85f
Mist-wu <2550166270@qq.com>
2025-12-18 22:43:48 +0800

    update

--- config/config.py
+++ config/config.py
@@ -14,10 +14,10 @@ deepseek_api_key = os.getenv("DEEPSEEK_API_KEY")
 napcat_token = os.getenv("NAPCAT_TOKEN")
 
 root_user = "2550166270"
-jiang = "3092760442"
+jiang_kai_yuan = "3092760442"
 
 group_whitelist = {1051660592}
-user_whitelist = {root_user, jiang}
+user_whitelist = {root_user, jiang_kai_yuan}
 user_blacklist = set()
 
 def is_user_allowed(user_id: str) -> bool:
